171abe69f2e2e527f939fabd4f963e4b73223e20
sapronov81-ctrl <sapronov81@gmail.com>
2025-11-10 07:49:02 +0000

    chore: cleanup expo cache and sync configs

--- babel.config.js
+++ babel.config.js
@@ -1,1 +1,6 @@
-module.exports=function(api){api.cache(true);return{presets:['babel-preset-expo'],plugins:[['module-resolver',{alias:{'@':'./src'}}],'react-native-reanimated/plugin']}};
+module.exports = function (api) {
+  api.cache(true);
+  return {
+    presets: ['babel-preset-expo'],
+  };
+};
