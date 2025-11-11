1790b8f402ee3d0db672306656e63c16ec533a92
sapronov81-ctrl <sapronov81@gmail.com>
2025-11-11 13:30:36 +0300

    Delete App.tsx

--- App.tsx
+++
@@ -1,1 +0,0 @@
-import 'react-native-gesture-handler';import React from 'react';import { NavigationContainer } from '@react-navigation/native';import Tabs from '@/navigation/Tabs';import { StatusBar } from 'expo-status-bar';export default function App(){return (<NavigationContainer><StatusBar style='dark'/><Tabs/></NavigationContainer>);}
